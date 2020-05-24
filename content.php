6db6836f5a726c7fea60beebc411e050f544edd6
shaphan <chaphannzabonimana@gmail.com>
2020-05-24 16:36:23 +0200

    convert a basic bootstrap blog theme to wordpress theme

---
+++ content.php
@@ -0,0 +1,12 @@
+<div class="blog-post">
+  <h2 class="blog-post-title"><?php the_title(); ?>t</h2>
+  <p class="blog-post-meta"><?php the_date() ?><a href="#"><?php the_author() ?></a></p>
+
+  <p>
+    <?php the_content() ?>
+  </p>
+  <hr />
+
+  <!-- the rest of the content -->
+</div>
+<!-- /.blog-post -->
